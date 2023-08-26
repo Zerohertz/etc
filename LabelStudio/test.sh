550ef90acb7e98a1e3574b1782178d37e8c2d3f6
Zerohertz <ohg3417@gmail.com>
2023-08-26 18:41:28 +0900

    :recycle: Remove: Label Studio

--- LabelStudio/test.sh
+++
@@ -1,7 +0,0 @@
-docker build -t label-studio .
-docker run \
-    --name label-studio \
-    -p 8080:8080 \
-    -v ${root}/data:/label-studio/data \
-    -v ${root}/files:/home/user \
-    label-studio
