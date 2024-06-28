8f103b14ec629dc46feaa2746727fb4cd8f00dac
Zerohertz <ohg3417@gmail.com>
2024-06-29 02:15:32 +0900

    :hammer: Update: Install Neovim

--- Util/sh/install_neovim.sh
+++ Util/sh/install_neovim.sh
@@ -1,4 +1,7 @@
-sudo add-apt-repository ppa:neovim-ppa/unstable
-sudo apt update
-sudo apt install neovim
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+sudo mv ./nvim.appimage /usr/bin/nvim
 
+# sudo add-apt-repository ppa:neovim-ppa/unstable
+# sudo apt update
+# sudo apt install neovim
