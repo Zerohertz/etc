06c5a8c6a655b3b28d503285e6cd3eeb5a430c80
Zerohertz <ohg3417@gmail.com>
2024-11-27 23:07:41 +0900

    :tada: add: init macos

---
+++ Util/sh/init_macos.sh
@@ -0,0 +1,33 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install kitty
+brew install git
+
+brew install --cask karabiner-elements
+
+brew install neovim
+brew install neovide
+brew install --cask font-hack-nerd-font
+brew install fd
+brew install ripgrep
+brew install lazygit
+brew install luarocks
+brew install node
+
+pip install neovim
+npm install -g neovim
+
+brew install --cask visual-studio-code
+
+brew install eza
+brew install tmux
+brew install --cask miniconda
+brew install go
+brew install rust
+
+brew install gnupg
+
+brew install --cask docker
+
+brew install --cask slack
+brew install --cask discord
+brew install --cask nextcloud
