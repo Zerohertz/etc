57b3c8ce0ab9149f065302a2d2fff80746c44f5e
Zerohertz <ohg3417@gmail.com>
2024-11-30 01:29:55 +0900

    :sparkles: update: init macos

--- Util/sh/init_macos.sh
+++ Util/sh/init_macos.sh
@@ -1,9 +1,24 @@
+# -------- Brew -------- #
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-brew install kitty
-brew install git
 
+# -------- Kitty -------- #
+brew install kitty
+
+# -------- Git -------- #
+brew install git
+brew install gnupg
+
+# -------- Karabiner -------- #
 brew install --cask karabiner-elements
 
+# -------- Dev -------- #
+brew install --cask miniconda
+brew install go
+brew install rust
+brew install node
+brew install --cask docker
+
+# -------- Neovim -------- #
 brew install neovim
 brew install neovide
 brew install --cask font-hack-nerd-font
@@ -11,23 +26,20 @@ brew install fd
 brew install ripgrep
 brew install lazygit
 brew install luarocks
-brew install node
 
+# -------- Neovim -------- #
 pip install neovim
 npm install -g neovim
 
+# -------- VSCode -------- #
 brew install --cask visual-studio-code
 
+# -------- CLI -------- #
 brew install eza
 brew install tmux
-brew install --cask miniconda
-brew install go
-brew install rust
+brew install speedtest-cli
 
-brew install gnupg
-
-brew install --cask docker
-
+# -------- Util -------- #
 brew install --cask slack
 brew install --cask discord
 brew install --cask nextcloud
